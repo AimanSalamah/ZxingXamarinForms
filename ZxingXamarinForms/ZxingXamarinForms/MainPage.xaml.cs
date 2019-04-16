30317d057a27d349dd1043096f6df0d51e3d10b8
Aiman Salamah <aiman@root.sa>
2019-04-16 20:23:27 +0300

    fix scan reading and show it in labe

--- ZxingXamarinForms/ZxingXamarinForms/MainPage.xaml.cs
+++ ZxingXamarinForms/ZxingXamarinForms/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
 
 namespace ZxingXamarinForms
 {
@@ -26,8 +27,8 @@ namespace ZxingXamarinForms
                 TranslationY = 120
             };
             //Attach the click event
+            var ScanResult = new Label();
             btnScan.Clicked += btnScan_Clicked;
-
             this.Content = new StackLayout
             {
                 BackgroundColor = Color.FromRgb(150, 172, 135),
@@ -35,45 +36,23 @@ namespace ZxingXamarinForms
                 Padding = 25,
                 Children =
             {
-                btnScan
+                btnScan,
+                ScanResult
             }
             };
             async void btnScan_Clicked(object sender, EventArgs e)
             {
-#if __ANDROID__
-	// Initialize the scanner first so it can track the current context
-	MobileBarcodeScanner.Initialize (Application);
-#endif
-
-                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
-                scanner.UseCustomOverlay = true;
-
-                var result = await scanner.Scan();
-
-                if (result != null)
-                    Console.WriteLine("Scanned Barcode: " + result.Text);
-
-                scanner.TopText = "Hold the camera up to the barcode\nAbout 6 inches away";
-                scanner.BottomText = "Wait for the barcode to automatically scan!";
-
-                //This will start scanning
-                 result = await scanner.Scan();
-
-                //Show the result returned.
-                HandleResult(result);
+                var scan = new ZXingScannerPage();
+                await this.Navigation.PushAsync(scan);
+                scan.OnScanResult += (eee) =>
+                 {
+                     Device.BeginInvokeOnMainThread(async () =>
+                     {
+                         await this.Navigation.PopAsync();
+                         ScanResult.Text = eee.Text;
+                     });
+                 };
             }
-
-            void HandleResult(ZXing.Result result)
-            {
-                var msg = "No Barcode!";
-                if (result != null)
-                {
-                    msg = "Barcode: " + result.Text + " (" + result.BarcodeFormat + ")";
-                }
-
-                DisplayAlert("", msg, "Ok");
-            }
-
         }
     }
 }
