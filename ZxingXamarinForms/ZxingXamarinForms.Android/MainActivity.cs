7be0f35f8bb48919c4b814906e5ae78ce6879b18
Aiman Salamah <aiman@root.sa>
2019-09-02 21:42:14 +0300

    remove and sort everything

--- ZxingXamarinForms/ZxingXamarinForms.Android/MainActivity.cs
+++ ZxingXamarinForms/ZxingXamarinForms.Android/MainActivity.cs
@@ -1,11 +1,7 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace ZxingXamarinForms.Droid
 {
@@ -16,7 +12,6 @@ namespace ZxingXamarinForms.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-            ZXing.Net.Mobile.Forms.Android.Platform.Init();
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
